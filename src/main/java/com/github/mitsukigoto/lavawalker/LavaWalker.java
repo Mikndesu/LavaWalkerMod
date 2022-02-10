a303a470eebb28867050ab6aea992530a278b898
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2022-02-11 02:05:16 +0900

    setup WorkSpace for Mixin

---
+++ src/main/java/com/github/mitsukigoto/lavawalker/LavaWalker.java
@@ -0,0 +1,24 @@
+package com.github.mitsukigoto.lavawalker;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(LavaWalker.MODID)
+public class LavaWalker {
+
+    public static final String MODID = "lava_walker";
+    public static final Logger LOGGER = LogManager.getLogger("LavaWalker/Main");
+
+    public LavaWalker() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+//        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, HisabisaConfig.SPEC, "hisabisamod-common.toml");
+//        EnchantmentInit.ENCHANTMENTS.register(bus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+}
---
+++ src/main/java/com/github/mitsukigoto/lavawalker/asm/mixin/FrostWalkerEnchantmentMixin.java
@@ -0,0 +1,9 @@
+package com.github.mitsukigoto.lavawalker.asm.mixin;
+
+import net.minecraft.world.item.enchantment.FrostWalkerEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(FrostWalkerEnchantment.class)
+public class FrostWalkerEnchantmentMixin {
+
+}
