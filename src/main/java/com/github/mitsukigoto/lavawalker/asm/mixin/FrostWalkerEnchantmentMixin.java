151b16d21f1ccadf5ad229bf5161119dc70225bf
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-02-13 02:22:49 +0900

    Lava Walker now exclusives with frost walker

--- src/main/java/com/github/mitsukigoto/lavawalker/asm/mixin/FrostWalkerEnchantmentMixin.java
+++ src/main/java/com/github/mitsukigoto/lavawalker/asm/mixin/FrostWalkerEnchantmentMixin.java
@@ -1,8 +1,23 @@
 package com.github.mitsukigoto.lavawalker.asm.mixin;
 
+import com.github.mitsukigoto.lavawalker.init.EnchantmentInit;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.enchantment.FrostWalkerEnchantment;
+import net.minecraft.inventory.EquipmentSlotType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FrostWalkerEnchantment.class)
-public class FrostWalkerEnchantmentMixin {
+public class FrostWalkerEnchantmentMixin extends Enchantment {
+    protected FrostWalkerEnchantmentMixin(Rarity p_i46731_1_, EnchantmentType p_i46731_2_, EquipmentSlotType[] p_i46731_3_) {
+        super(p_i46731_1_, p_i46731_2_, p_i46731_3_);
+    }
+    @Inject(method = "checkCompatibility(Lnet/minecraft/enchantment/Enchantment;)Z", at=@At("RETURN"), cancellable = true)
+    private void inject(Enchantment p_77326_1_, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(super.checkCompatibility(p_77326_1_) && p_77326_1_ != Enchantments.DEPTH_STRIDER && p_77326_1_ != EnchantmentInit.LAVA_WALKER.get());
+    }
 }
