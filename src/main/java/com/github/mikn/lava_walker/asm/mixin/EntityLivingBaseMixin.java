7bc70e61278a80a78d9002bc310197c2061768e9
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-02-16 12:28:26 +0900

    lava walker now affects gameplay

---
+++ src/main/java/com/github/mikn/lava_walker/asm/mixin/EntityLivingBaseMixin.java
@@ -0,0 +1,30 @@
+package com.github.mikn.lava_walker.asm.mixin;
+
+import com.github.mikn.lava_walker.LavaWalker;
+import com.github.mikn.lava_walker.enchantment.LavaWalkerEnchantment;
+import net.minecraft.enchantment.EnchantmentFrostWalker;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Enchantments;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EntityLivingBase.class)
+public class EntityLivingBaseMixin {
+    @Redirect(method= "onEntityUpdate()V", at=@At(value="INVOKE", target="Lnet/minecraft/entity/EntityLivingBase;frostWalk(Lnet/minecraft/util/math/BlockPos;)V"))
+    private void redirect(EntityLivingBase instance, BlockPos pos) {
+        EntityLivingBase entityLivingBase = (EntityLivingBase)(Object)this;
+        int k = EnchantmentHelper.getMaxEnchantmentLevel(LavaWalker.LAVA_WAKER, entityLivingBase);
+        if (k > 0)
+        {
+            LavaWalkerEnchantment.freezeNearby(entityLivingBase, entityLivingBase.world, pos, k);
+        }
+        int i = EnchantmentHelper.getMaxEnchantmentLevel(Enchantments.FROST_WALKER, entityLivingBase);
+        if (i > 0)
+        {
+            EnchantmentFrostWalker.freezeNearby(entityLivingBase, entityLivingBase.world, pos, i);
+        }
+    }
+}
