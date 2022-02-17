20ada2d3e0786b927d3db1372048405bd9592b62
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-02-17 21:58:52 +0900

    implement most of lavawalker funnc and fix modded_obsidian texture issue

--- src/main/java/com/github/mikn/lava_walker/block/ModdedObsidian.java
+++ src/main/java/com/github/mikn/lava_walker/block/ModdedObsidian.java
@@ -4,13 +4,80 @@ import com.github.mikn.lava_walker.LavaWalker;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class ModdedObsidian extends Block {
+
+    public static final PropertyInteger AGE_1 = PropertyInteger.create("age", 0, 1);
+
     public ModdedObsidian() {
         super(Material.ROCK);
         this.setHardness(50.0F).setResistance(2000.0F).setSoundType(SoundType.STONE);
         this.setRegistryName(LavaWalker.MODID, "modded_obsidian");
         this.setTranslationKey("modded_obsidian");
+        this.setDefaultState(this.blockState.getBaseState().withProperty(AGE_1, 0));
     }
+
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(AGE_1);
+    }
+
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(AGE_1, MathHelper.clamp(meta, 0, 3));
+    }
+
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        if (rand.nextInt(3) == 0 )
+        {
+            this.slightlyMelt(worldIn, pos, state, rand);
+        }
+        else
+        {
+            worldIn.scheduleUpdate(pos, this, MathHelper.getInt(rand, 20, 40));
+        }
+    }
+
+    protected void slightlyMelt(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        int i = (Integer) state.getValue(AGE_1);
+
+        if (i < 1)
+        {
+            worldIn.setBlockState(pos, state.withProperty(AGE_1, i + 1), 2);
+            worldIn.scheduleUpdate(pos, this, MathHelper.getInt(rand, 20, 40));
+        }
+        else
+        {
+            this.turnIntoLava(worldIn, pos);
+        }
+    }
+
+    protected void turnIntoLava(World worldIn, BlockPos pos)
+    {
+        worldIn.setBlockState(pos, Blocks.LAVA.getDefaultState());
+    }
+
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {AGE_1});
+    }
+
+    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
+    {
+        return ItemStack.EMPTY;
+    }
+
 }
--- src/main/java/com/github/mikn/lava_walker/enchantment/LavaWalkerEnchantment.java
+++ src/main/java/com/github/mikn/lava_walker/enchantment/LavaWalkerEnchantment.java
@@ -1,5 +1,6 @@
 package com.github.mikn.lava_walker.enchantment;
 
+import com.github.mikn.lava_walker.LavaWalker;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -62,10 +63,10 @@ public class LavaWalkerEnchantment extends Enchantment
                     {
                         IBlockState iblockstate1 = worldIn.getBlockState(blockpos$mutableblockpos1);
 
-                        if (iblockstate1.getMaterial() == Material.WATER && (iblockstate1.getBlock() == net.minecraft.init.Blocks.WATER || iblockstate1.getBlock() == net.minecraft.init.Blocks.FLOWING_WATER) && ((Integer)iblockstate1.getValue(BlockLiquid.LEVEL)).intValue() == 0 && worldIn.mayPlace(Blocks.FROSTED_ICE, blockpos$mutableblockpos1, false, EnumFacing.DOWN, (Entity)null))
+                        if (iblockstate1.getMaterial() == Material.LAVA && (iblockstate1.getBlock() == net.minecraft.init.Blocks.LAVA || iblockstate1.getBlock() == Blocks.FLOWING_LAVA) && ((Integer)iblockstate1.getValue(BlockLiquid.LEVEL)).intValue() == 0 && worldIn.mayPlace(LavaWalker.MODDED_OBSIDIAN, blockpos$mutableblockpos1, false, EnumFacing.DOWN, (Entity)null))
                         {
-                            worldIn.setBlockState(blockpos$mutableblockpos1, Blocks.FROSTED_ICE.getDefaultState());
-                            worldIn.scheduleUpdate(blockpos$mutableblockpos1.toImmutable(), Blocks.FROSTED_ICE, MathHelper.getInt(living.getRNG(), 60, 120));
+                            worldIn.setBlockState(blockpos$mutableblockpos1, LavaWalker.MODDED_OBSIDIAN.getDefaultState());
+                            worldIn.scheduleUpdate(blockpos$mutableblockpos1.toImmutable(), LavaWalker.MODDED_OBSIDIAN, MathHelper.getInt(living.getRNG(), 20, 40));
                         }
                     }
                 }
