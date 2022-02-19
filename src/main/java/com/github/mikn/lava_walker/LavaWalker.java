edd33e16de9f9a1335c90adcb4f162d7e99cd55d
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-02-20 02:08:40 +0900

    add support for Forge Version Checker

--- src/main/java/com/github/mikn/lava_walker/LavaWalker.java
+++ src/main/java/com/github/mikn/lava_walker/LavaWalker.java
@@ -2,26 +2,34 @@ package com.github.mikn.lava_walker;
 
 import com.github.mikn.lava_walker.block.ModdedObsidian;
 import com.github.mikn.lava_walker.enchantment.LavaWalkerEnchantment;
+import ibxm.Player;
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.libraries.ModList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = LavaWalker.MODID)
+@Mod(modid = LavaWalker.MODID, updateJSON = "https://raw.githubusercontent.com/MitsukiGoto/LavaWalkerMod/1.12.2/versions/versions.json")
 public class LavaWalker {
     public static final String MODID = "lava_walker";
     public static final Logger LOGGER = LogManager.getLogger("LavaWalker/Main");
@@ -47,6 +55,16 @@ public class LavaWalker {
     @SubscribeEvent
     public void registerItems(RegistryEvent.Register<Item> evt) {
         evt.getRegistry().register(new ItemBlock(MODDED_OBSIDIAN).setRegistryName(MODDED_OBSIDIAN.getRegistryName()).setTranslationKey("modded_obsidian"));
+    }
+
+    @SubscribeEvent
+    public void updateCheck(PlayerEvent.PlayerLoggedInEvent evt) {
+        ModContainer container = Loader.instance().getIndexedModList().get(MODID);
+        ForgeVersion.Status status = ForgeVersion.getResult(container).status;
+        LOGGER.info(status);
+        if(status == ForgeVersion.Status.OUTDATED) {
+            Minecraft.getMinecraft().player.sendChatMessage("LavaWalker Mod: New Version Available!");
+        }
     }
 
     @SubscribeEvent
