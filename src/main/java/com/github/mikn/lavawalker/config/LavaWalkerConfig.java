2863e343b3f1dc73ab02817b9f41b578290ce81e
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-05-16 07:29:39 +0900

    rewrite LavaWalkerConfig.java so that you can use Enum value, adjust other classes to it

--- src/main/java/com/github/mikn/lavawalker/config/LavaWalkerConfig.java
+++ src/main/java/com/github/mikn/lavawalker/config/LavaWalkerConfig.java
@@ -3,12 +3,26 @@ package com.github.mikn.lavawalker.config;
 import com.github.mikn.lavawalker.LavaWalker;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.RequiresRestart;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.TransitiveObject;
+import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 
 @Config(name = LavaWalker.MODID)
-public class LavaWalkerConfig implements ConfigData {
-    public boolean isTreasure = true;
-    public int maxEnchantmentLevel = 2;
-    public enum vanishmentSpeedEnum {
+public class LavaWalkerConfig extends PartitioningSerializer.GlobalData {
+    @ConfigEntry.Category("module")
+    @TransitiveObject
+    public Module module = new Module();
+    enum VanishmentSpeedEnum {
         SLOW, NORMAL, FAST
+    }
+
+    @Config(name = "module")
+    public static class Module implements ConfigData {
+        public boolean isTreasure = true;
+        @RequiresRestart
+        public int maxEnchantmentLevel = 2;
+        @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
+        private VanishmentSpeedEnum anEnumWithButton = VanishmentSpeedEnum.NORMAL;
     }
 }
