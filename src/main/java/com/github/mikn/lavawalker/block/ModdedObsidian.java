ac062e914caf84f76e9b4ef401855ac68c7d935a
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-05-17 18:49:34 +0900

    use own IntegerProperty instead of vanilla's one

--- src/main/java/com/github/mikn/lavawalker/block/ModdedObsidian.java
+++ src/main/java/com/github/mikn/lavawalker/block/ModdedObsidian.java
@@ -9,19 +9,20 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-
-import static net.minecraft.world.level.block.state.properties.BlockStateProperties.AGE_3;
 
 import com.github.mikn.lavawalker.LavaWalker;
 
 public class ModdedObsidian extends Block {
 
+    private static final IntegerProperty AGE = IntegerProperty.create("age", 0, 3);
+
     public ModdedObsidian() {
         super(Properties.of(Material.STONE, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
                 .strength(50.0f, 1200.0f).lightLevel((p_235435_0_) -> 10));
-        this.registerDefaultState(this.stateDefinition.any().setValue(AGE_3, Integer.valueOf(1)));
+        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, Integer.valueOf(1)));
     }
 
     public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
@@ -32,14 +33,14 @@ public class ModdedObsidian extends Block {
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AGE_3);
+        builder.add(AGE);
     }
 
     private boolean slightlyMelt(BlockState blockState, Level level, BlockPos blockPos) {
         final int MAX_AGE_BEFORE_LAVA = 2;
-        int i = blockState.getValue(AGE_3);
+        int i = blockState.getValue(AGE);
         if (i < MAX_AGE_BEFORE_LAVA) {
-            level.setBlock(blockPos, blockState.setValue(AGE_3, Integer.valueOf(i + 1)), 2);
+            level.setBlock(blockPos, blockState.setValue(AGE, Integer.valueOf(i + 1)), 2);
             return false;
         } else {
             this.melt(blockState, level, blockPos);
