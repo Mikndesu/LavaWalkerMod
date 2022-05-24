3fbe51b0a1fc598e28594d9849ab80c2be36eca5
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-05-24 10:47:20 +0900

    remove networking system and toggle system

--- src/main/java/com/github/mikn/lavawalker/LavaWalker.java
+++ src/main/java/com/github/mikn/lavawalker/LavaWalker.java
@@ -4,15 +4,12 @@ import com.github.mikn.lavawalker.config.LavaWalkerConfig;
 import com.github.mikn.lavawalker.init.BlockInit;
 import com.github.mikn.lavawalker.init.EnchantmentInit;
 import com.github.mikn.lavawalker.init.ItemInit;
-import com.github.mikn.lavawalker.network.Message;
-import com.github.mikn.lavawalker.network.NetWork;
-import net.minecraft.client.KeyMapping;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -23,15 +20,7 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.VersionChecker;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.lwjgl.glfw.GLFW;
-
-import java.util.ArrayList;
-import java.util.UUID;
 
 
 @Mod(LavaWalker.MODID)
@@ -39,9 +28,6 @@ public class LavaWalker {
 
     public static final String MODID = "lava_walker";
     public static final Logger LOGGER = LogManager.getLogger("LavaWalker/Main");
-    public static KeyMapping[] keyMappings;
-    public static ArrayList<UUID> availablePlayers;
-    private boolean isEnchantmentAvailable = true;
 
     public LavaWalker() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -49,33 +35,7 @@ public class LavaWalker {
         BlockInit.BLOCKS.register(bus);
         ItemInit.ITEMS.register(bus);
         EnchantmentInit.ENCHANTMENTS.register(bus);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        keyMappings = new KeyMapping[1];
-        keyMappings[0] = new KeyMapping("Enable/Disable LavaWalker", GLFW.GLFW_KEY_J, "LavaWalker");
-        ClientRegistry.registerKeyBinding(keyMappings[0]);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent evt) {
-        NetWork.init();
-        availablePlayers = new ArrayList<>();
-    }
-
-    @SubscribeEvent
-    public void toggleAffect(InputEvent.KeyInputEvent evt) {
-        if(keyMappings[0].isDown()) {
-            if(isEnchantmentAvailable) {
-                sendClientMessage("LavaWalker Enchantment is unavailable now");
-            } else {
-                sendClientMessage("LavaWalker Enchantment is available now");
-            }
-            NetWork.CHANNEL.sendToServer(new Message(1));
-            isEnchantmentAvailable = !isEnchantmentAvailable;
-        }
     }
 
     @SubscribeEvent
