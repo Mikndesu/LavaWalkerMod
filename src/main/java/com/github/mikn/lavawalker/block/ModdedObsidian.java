a2aebdbfb6b8bfc555b6b4afeaf2c6d31c3c3b27
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-05-26 09:28:01 +0900

    checkout ModdedObsidian.java

--- src/main/java/com/github/mikn/lavawalker/block/ModdedObsidian.java
+++ src/main/java/com/github/mikn/lavawalker/block/ModdedObsidian.java
@@ -1,43 +1,45 @@
 package com.github.mikn.lavawalker.block;
 
-import com.github.mikn.lavawalker.init.BlockInit;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 
 import java.util.Random;
 
-import static net.minecraft.world.level.block.state.properties.BlockStateProperties.AGE_1;
-
 public class ModdedObsidian extends Block {
 
+    private static final IntegerProperty AGE = IntegerProperty.create("age", 0, 3);
+
     public ModdedObsidian() {
-        super(Properties.of(Material.STONE, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops().strength(50.0f, 1200.0f).lightLevel((p_235435_0_) -> 10));
-        this.registerDefaultState(this.stateDefinition.any().setValue(AGE_1, Integer.valueOf(1)));
+        super(Properties.of(Material.STONE, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
+                .strength(50.0f, 1200.0f).lightLevel((p_235435_0_) -> 10));
+        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, Integer.valueOf(1)));
     }
 
     public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, Random random) {
-        if ((random.nextInt(3) == 0 && this.slightlyMelt(blockState, serverLevel, blockPos))) {
-
-        } else {
+        int meltProbability = 3;
+        if (!((random.nextInt(meltProbability) == 0 && this.slightlyMelt(blockState, serverLevel, blockPos)))) {
             serverLevel.scheduleTick(blockPos, this, Mth.nextInt(random, 20, 40));
         }
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AGE_1);
+        builder.add(AGE);
     }
 
     private boolean slightlyMelt(BlockState blockState, Level level, BlockPos blockPos) {
-        int i = blockState.getValue(AGE_1);
-        if (i < 1) {
-            level.setBlock(blockPos, blockState.setValue(AGE_1, Integer.valueOf(i + 1)), 2);
+        final int MAX_AGE_BEFORE_LAVA = 2;
+        int i = blockState.getValue(AGE);
+        if (i < MAX_AGE_BEFORE_LAVA) {
+            level.setBlock(blockPos, blockState.setValue(AGE, Integer.valueOf(i + 1)), 2);
             return false;
         } else {
             this.melt(blockState, level, blockPos);
@@ -46,8 +48,7 @@ public class ModdedObsidian extends Block {
     }
 
     private void melt(BlockState blockState, Level level, BlockPos blockPos) {
-        level.setBlockAndUpdate(blockPos, BlockInit.MODDED_CRYING_OBSIDIAN.get().defaultBlockState());
-        level.scheduleTick(blockPos, BlockInit.MODDED_CRYING_OBSIDIAN.get(), Mth.nextInt(level.getRandom(), 20, 40));
+        level.setBlockAndUpdate(blockPos, Blocks.LAVA.defaultBlockState());
     }
 
 }
