a48e58245d24f693aa221e4960a79aea667d7782
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-07-14 23:22:15 +0900

    use congigured rarity

--- src/main/java/com/github/mikn/lavawalker/init/EnchantmentInit.java
+++ src/main/java/com/github/mikn/lavawalker/init/EnchantmentInit.java
@@ -1,9 +1,10 @@
 package com.github.mikn.lavawalker.init;
 
+import com.github.mikn.lavawalker.LavaWalker;
 import com.github.mikn.lavawalker.enchantment.LavaWalkerEnchantment;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 
 public class EnchantmentInit {
-    public static final Enchantment LAVA_WALKER = new LavaWalkerEnchantment(Enchantment.Rarity.RARE, EquipmentSlot.FEET);
+    public static final Enchantment LAVA_WALKER = new LavaWalkerEnchantment(LavaWalker.HOLDER.getConfig().rarity, EquipmentSlot.FEET);
 }
