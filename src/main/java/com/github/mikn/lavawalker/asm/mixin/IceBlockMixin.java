fcdda1276385f77c41d439df23efafed3fd662fc
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2022-11-03 00:46:45 +0900

    when ice block being near lava, it will turn to be lava

---
+++ src/main/java/com/github/mikn/lavawalker/asm/mixin/IceBlockMixin.java
@@ -0,0 +1,28 @@
+package com.github.mikn.lavawalker.asm.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.IceBlock;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.github.mikn.lavawalker.LavaWalker;
+
+@Mixin(IceBlock.class)
+public class IceBlockMixin {
+    @Inject(method = "melt(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V", at = @At("HEAD"), cancellable = true)
+    private void inject(BlockState state, Level level, BlockPos blockPos, CallbackInfo ci) {
+        if (LavaWalker.HOLDER.isCompatibleWithBedrock) {
+            BlockState blockState = level.getBlockState(blockPos.below());
+            if (blockState.is(Blocks.LAVA)) {
+                level.setBlockAndUpdate(blockPos, Blocks.LAVA.defaultBlockState());
+                ci.cancel();
+            }
+        }
+    }
+}
