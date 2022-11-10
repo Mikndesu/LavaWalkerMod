64716dd438911edae764036e91359b1ad9326879
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2022-11-11 03:06:53 +0900

    stop using events and directly affect behaviour

--- src/main/java/com/github/mikn/lavawalker/asm/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/mikn/lavawalker/asm/mixin/LivingEntityMixin.java
@@ -1,19 +1,44 @@
+/*
+ Copyright (c) 2022 Mikndesu
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.github.mikn.lavawalker.asm.mixin;
 
-import com.github.mikn.lavawalker.event.OnChangedBlockEvent;
+import com.github.mikn.lavawalker.enchantment.LavaWalkerEnchantment;
+import com.github.mikn.lavawalker.init.EnchantmentInit;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
+public class LivingEntityMixin {
     @Inject(method = "onChangedBlock(Lnet/minecraft/util/math/BlockPos;)V", at = @At("HEAD"))
     private void inject(BlockPos blockPos, CallbackInfo ci) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
-        MinecraftForge.EVENT_BUS.post(new OnChangedBlockEvent(blockPos, livingEntity));
+        int k = EnchantmentHelper.getEnchantmentLevel(EnchantmentInit.LAVA_WALKER.get(), livingEntity);
+        if (k > 0) {
+            LavaWalkerEnchantment.onEntityMoved(livingEntity, livingEntity.level, blockPos, k);
+        }
     }
 }
