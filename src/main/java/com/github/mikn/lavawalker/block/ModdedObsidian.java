72828fe430cac3d12824ee3d162af80eb339bf12
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2023-07-17 23:43:34 +0900

    Remove isMinable config

--- src/main/java/com/github/mikn/lavawalker/block/ModdedObsidian.java
+++ src/main/java/com/github/mikn/lavawalker/block/ModdedObsidian.java
@@ -48,15 +48,6 @@ public class ModdedObsidian extends Block {
         this.registerDefaultState(this.stateDefinition.any().setValue(AGE, Integer.valueOf(1)));
     }
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public float getDestroyProgress(BlockState p_60466_, Player p_60467_, BlockGetter p_60468_, BlockPos p_60469_) {
-        // This value is the same as that of obsidian
-        final float HARDNESS = 50.0f;
-        return LavaWalker.HOLDER.isMinable ? HARDNESS
-                : super.getDestroyProgress(p_60466_, p_60467_, p_60468_, p_60469_);
-    }
-
     public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
         int meltProbability = LavaWalker.HOLDER.meltSpeed.getInt();
         if (!((randomSource.nextInt(meltProbability) == 0 && this.slightlyMelt(blockState, serverLevel, blockPos)))) {
